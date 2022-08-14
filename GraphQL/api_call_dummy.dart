7d18587f069c17875e90734b469fbcbc04c4f871
Aniket Yadav <aniket4897@gmail.com>
2022-08-14 12:28:42 +0530

    api call

---
+++ GraphQL/api_call_dummy.dart
@@ -0,0 +1,53 @@
+void fetchData() async {
+    setState(() {
+      _loading = true; 
+    });
+  
+  
+   HttpLink httpLink = HttpLink("https://rickandmortyapi.com/graphql");
+   AuthLink authLink = AuthLink(
+    getToken: () async => 'Bearer ${await SessionManager.getAuthToken()}',
+  );
+
+   AuthLink apiKey = AuthLink(
+    getToken: () async => await SessionManager.getAPI(),
+    headerKey: 'apiKey',
+  );
+  Link link = authLink.concat(apiKey).concat(httpLink);
+  
+    GraphQLClient qlClient = GraphQLClient(
+      
+      link: link,
+      cache: GraphQLCache(
+        store:
+            HiveStore(),
+      ),
+    );
+    QueryResult queryResult = await qlClient.query(
+      QueryOptions(
+        document: gql(
+          """query {
+  characters() {
+    results {
+      name
+      image 
+    }
+  }
+  
+}""",
+        ),
+      ),
+    );
+
+// queryResult.data  // contains data
+// queryResult.exception // will give what exception you got /errors
+// queryResult.hasException // you can check if you have any exception
+
+// queryResult.context.entry<HttpLinkResponseContext>()?.statusCode  // to get status code of response
+
+    setState(() {
+      characters = queryResult.data!['characters'][
+          'results']; 
+      _loading = false;
+    });
+  }
