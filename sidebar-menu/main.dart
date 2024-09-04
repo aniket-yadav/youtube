921c704d94328784104dd95378debec3ac99a35c
Aniket Yadav <aniket4897@gmail.com>
2024-09-04 12:06:19 +0530

    Create main.dart

---
+++ sidebar-menu/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:testhero/dashboard.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Dashboard(),
+    );
+  }
+}
