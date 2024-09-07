321fb914669df66cbf15c929d61e41cd7810ab6c
Aniket Yadav <aniket4897@gmail.com>
2024-09-07 10:20:54 +0530

    Create product.dart

---
+++ hero-2024/model/product.dart
@@ -0,0 +1,37 @@
+import 'package:testhero/model/review.dart';
+
+class Product {
+  int? id;
+  String? title;
+  String? description;
+  String? category;
+  double? price;
+  double? rating;
+  String? brand;
+  List<Review>? reviews;
+  String? thumbnail;
+  Product({
+    this.brand,
+    this.category,
+    this.description,
+    this.id,
+    this.price,
+    this.rating,
+    this.reviews,
+    this.title,
+    this.thumbnail,
+  });
+
+  factory Product.fromJson(Map<String, dynamic>? json) => Product(
+        brand: json?['brand'],
+        category: json?['category'],
+        description: json?['description'],
+        id: json?['id'],
+        price: json?['price'],
+        rating: json?['rating'],
+        reviews:
+            json?['reviews']?.map<Review>((e) => Review.fromJson(e)).toList(),
+        title: json?['title'],
+        thumbnail: json?['thumbnail'],
+      );
+}
