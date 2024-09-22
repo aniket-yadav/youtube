86dd65a5fc88ea87e38ae0942e66ff4354c8428e
Aniket Yadav <aniket4897@gmail.com>
2024-09-22 22:03:03 +0530

    Create main.dart

---
+++ search-delegates/main.dart
@@ -0,0 +1,17 @@
+//http: ^1.2.2 add this plugin
+import 'package:dummy/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Home(),
+    );
+  }
+}
